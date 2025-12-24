fa1e5502b6f44de5518d5384260a69a8fe31a912
amangupta2509 <amangupta.t6@gmail.com>
2025-12-24 13:38:41 +0530

    genetic paramaeter textboc removed

--- components/admin/genetic-parameters-admin.tsx
+++ components/admin/genetic-parameters-admin.tsx
@@ -4,7 +4,7 @@ import React, { JSX, useCallback, useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Textarea } from "@/components/ui/textarea";
+
 import {
   Select,
   SelectContent,
