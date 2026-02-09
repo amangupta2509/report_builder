9bb2bd7da7a3f96c378eef60335bd3db88fd008d
amangupta2509 <amangupta.t6@gmail.com>
2026-02-09 10:27:54 +0530

    use mobile.tsk modification

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -6,7 +6,9 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-   (window.innerWidth < MOBILE_BREAKPOINT)
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
