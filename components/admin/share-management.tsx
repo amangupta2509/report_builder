7695cc144112a881a9c4577a86982b2826109f0a
amangupta2509 <amangupta.t6@gmail.com>
2026-02-20 10:59:41 +0530

    share management

--- components/admin/share-management.tsx
+++ components/admin/share-management.tsx
@@ -13,7 +13,13 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Share2,
   Copy,
