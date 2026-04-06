ffa60be2c8361947adc6b5e94c94c4dd6a2b2178
amangupta2509 <amangupta.t6@gmail.com>
2026-04-06 17:19:11 +0530

    fix: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <button
         onClick={() => {
           alert("CLICKED");
-          window.location.href = "/login";
+          window.location.href = "/admin?tab=preview";
         }}
         style={{
           padding: "20px",
